885ae433c58273d4e7509410255f246de60d21c9
Mreljose <joseantonioleonlopez206@gmail.com>
2024-02-26 11:35:33 +0100

    Conectar con la base de datos

---
+++ src/main/java/com/helen/demo/HelenApplication.java
@@ -0,0 +1,13 @@
+package com.helen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelenApplication.class, args);
+	}
+
+}
