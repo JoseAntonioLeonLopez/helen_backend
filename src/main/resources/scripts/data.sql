470ae43e96009e68726681f218c1a8f429716e27
Jose Antonio Leon Lopez <joseantonioleonlopez206@gmail.com>
2024-04-23 08:31:08 +0200

    datos pruebas cambiado contraseña

--- src/main/resources/scripts/data.sql
+++ src/main/resources/scripts/data.sql
@@ -3,5 +3,5 @@ INSERT IGNORE INTO role (role, description) VALUES
 ('USER', 'Rol con permisos de usuario');
 
 INSERT IGNORE INTO user (username, name, first_surname, second_surname, gender, email, password, city, id_role) VALUES
-('administrador', 'Pepe', 'Leon', 'Lopez', 1, 'admin@example.com', '$2y$10$IFh5QANI3jmFHk5zi1Grw.B/3JXwqEAi9wHedoFRUh.2yQ4MV5axu', 'Sevilla', 1),
-('usuario', 'Lucia', 'Fuentes', 'Leon', 2, 'user@example.com', '$2y$10$IFh5QANI3jmFHk5zi1Grw.B/3JXwqEAi9wHedoFRUh.2yQ4MV5axu', 'Sevilla', 2);
+('administrador', 'Pepe', 'Leon', 'Lopez', 1, 'admin@example.com', '$2y$10$ReSLLFKW6qa8vtI8prN7o.CljGlRHH2uj5R0cKLIklQYToWCXRKFu', 'Sevilla', 1),
+('usuario', 'Lucia', 'Fuentes', 'Leon', 2, 'user@example.com', '$2y$10$ReSLLFKW6qa8vtI8prN7o.CljGlRHH2uj5R0cKLIklQYToWCXRKFu', 'Sevilla', 2);
