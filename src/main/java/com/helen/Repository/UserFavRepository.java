331ef8be25f40c337930a699d59ddde7133a1af9
Jose Antonio Leon Lopez <joseantonioleonlopez206@gmail.com>
2024-05-02 13:50:32 +0200

    arreglado bug borrar publicacion con me gustas

--- src/main/java/com/helen/Repository/UserFavRepository.java
+++ src/main/java/com/helen/Repository/UserFavRepository.java
@@ -10,4 +10,5 @@ public interface UserFavRepository extends JpaRepository<UserFav, Long> {
 
     Optional<UserFav> findByFkUserAndFkPublication(Long fkUser, Long fkPublication);
     Optional<UserFav> deleteByFkUserAndFkPublication(Long fkUser, Long fkPublication);
+	void deleteByFkPublication(Long id);
 }
--- src/main/java/com/helen/Service/PublicationService.java
+++ src/main/java/com/helen/Service/PublicationService.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 
 import com.helen.Entity.Publication;
 import com.helen.Repository.PublicationRepository;
+import com.helen.Repository.UserFavRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,6 +20,10 @@ public class PublicationService {
 
 	@Autowired
 	private final PublicationRepository publicationRepository;
+	
+	@Autowired
+	private final UserFavRepository userFavRepository;
+	
 	
 	public List<Publication> getAllPublications() {
 		return this.publicationRepository.findAll();
@@ -40,11 +46,17 @@ public class PublicationService {
         return publicationRepository.save(publication);
     }
 
+    @Transactional
     public boolean removePublication(Long id) {
     	return getPublication(id).map(publication -> {
-			publicationRepository.deleteById(id);
-			return true;
-		}).orElse(false);
+            // Eliminar los registros de user_fav asociados a la publicación
+            userFavRepository.deleteByFkPublication(id);
+            
+            // Eliminar la publicación
+            publicationRepository.deleteById(id);
+            
+            return true;
+        }).orElse(false);
     }
     
     //Metodo para sacar las 10 publicaciones con mas me gustas
